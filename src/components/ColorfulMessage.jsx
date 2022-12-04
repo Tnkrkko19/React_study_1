311ae4aa6c31ce9542a6ce9bf4c44a44ec1f5ce7
__rkko <rkkotnk219@gmail.com>
2022-12-04 02:07:54 +0000

    Update App.jsx and ColorfulMessage.jsx

--- src/components/ColorfulMessage.jsx
+++ src/components/ColorfulMessage.jsx
@@ -1,15 +1,16 @@
 import React from "react";
 
-const ColorfulMessage = (props) => {
+export const ColorfulMessage = (props) => {
   const { color, children } = props;
   const contentStyle = {
     // 同じ関数名であれば、省略可能
     // color: color,
     // fontSize: "18px"
     color,
-    fontSize: "18px"
+    fontSize: "18px",
   };
   return <p style={contentStyle}>{children}</p>;
 };
 
-export default ColorfulMessage;
+// constをexportで使用できるようにできるから。
+// export default ColorfulMessage;
